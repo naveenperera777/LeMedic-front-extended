4b0bfd416f707c83f2df2354d18b57e6c381bc57
Naveen Perera <naveenperera777@gmail.com>
2019-12-19 15:53:54 +0530

    dynamic reports by type selection

--- src/views/PatientStatistics.js
+++ src/views/PatientStatistics.js
@@ -3,48 +3,67 @@ import { Grid, Row, Col,ListGroup,ListGroupItem,MenuItem,DropdownButton } from "
 import {Bar} from 'react-chartjs-2';
 import axios from 'axios';
 import BarChart from '../components/Reports/BarChart';
-
+// CSS 57.9%	 JavaScript 41.5%	 HTML 0.6%
 
 export default function PatientStatistics() {
 
     const [gender, setGender] = useState("select gender");
     const [genderCount, setgenderCount] = useState({});
-    // const [diseaseList, setDiseaseList] = useState([]);
     const [diseaseListWithCount, setdiseaseListWithCount] = useState({});
     const [disease, setDisease] = useState("Select Disease");
+    const [selectedCategory,setSelectedCategory] = useState([]);
 
     let label = [];
     let graphData = [];
-    if(disease =="All"){
-      delete diseaseListWithCount.All;
-      for (let [key, value] of Object.entries(diseaseListWithCount)) {
-        console.log("key is --->",key, value);
-        label.push(key);
-        graphData.push(value);
+    if(selectedCategory.length == 1 ){
+      let category = selectedCategory[0];
+      if(category == "disease"){
+      switch(disease){
+        case "All":
+          delete diseaseListWithCount.All;
+          delete diseaseListWithCount.None;
+          for (let [key, value] of Object.entries(diseaseListWithCount)) {
+            console.log("key is --->",key, value);
+            label.push(key);
+            graphData.push(value);
+            }
+            diseaseListWithCount["All"] = 0; 
+            diseaseListWithCount["None"] = 0; 
+            break; 
+        case "None":
+          break;
+        default:
+          label.push(disease);
+          graphData.push(diseaseListWithCount[disease]);
+  
+          }
+        } else {   
+          
+          switch(gender){
+            case "All":
+              delete genderCount.All;
+              delete genderCount.None;
+              for (let [key, value] of Object.entries(genderCount)) {
+                console.log("key is --->",key, value);
+                label.push(key);
+                graphData.push(value);
+                }
+                genderCount["All"] = 0; 
+                genderCount["None"] = 0; 
+                break; 
+            case "None":
+              break;
+            default:
+              label.push(gender);
+              graphData.push(genderCount[gender]);
+      
+              } 
+            }
     }
-    diseaseListWithCount["All"] = 0;   
-    } else {
-      label.push(disease);
-      graphData.push(diseaseListWithCount[disease]);
-    }
+
+  
 
     console.log("graph data",graphData);
-
-     const data = {
-        labels  : label,
-        datasets: [
-            {
-                label               : 'Patient Count',
-                backgroundColor     : 'rgba(255,99,132,0.2)',
-                borderColor         : 'rgba(255,99,132,1)',
-                borderWidth         : 1,
-                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                hoverBorderColor    : 'rgba(255,99,132,1)',
-                data                : graphData
-            }
-        ]
-    };
-    
 
     useEffect(() => {
         const fetchGenderCount = async () => {
@@ -52,6 +71,8 @@ export default function PatientStatistics() {
             `http://localhost:9090/statistics/patient/count/gender`
           );
           let genderArr = result.data.data;
+          genderArr.push({"gender":"All","total":0});
+          genderArr.push({"gender":"None","total":0});
           console.log("genderArr",genderArr);
           for( let i=0 ; i < genderArr.length; i++){
             let genderItem = genderArr[i];
@@ -66,6 +87,7 @@ export default function PatientStatistics() {
           );
           let diseaseArr = result.data.data;
           diseaseArr.push({"disease": "All", "total": 0});
+          diseaseArr.push({"disease": "None", "total": 0});
           console.log("diseaseArr",diseaseArr);
           for( let i=0 ; i < diseaseArr.length; i++){
             let disease = diseaseArr[i];
@@ -74,41 +96,39 @@ export default function PatientStatistics() {
           }
         };
 
-        // const fetchDiseaseList = async () => {
-        //   const result = await axios(
-        //     `http://localhost:9090/statistics/patient/disease/list`
-        //   );
-        //   let alldieaseList = result.data.data;
-        //   console.log("dis------->",alldieaseList);
-        //   alldieaseList.push("All")
-        //   setDiseaseList(alldieaseList);
-        // };
-
+      
         fetchDiseaseListWithCount();
         fetchGenderCount();
-        // fetchDiseaseList();
+
       }, []);
   
 
-    function handleDiseaseChange(value){
-      setDisease(value);      
+    function handleDiseaseChange(type,value){
+      setDisease(value);
+      if(selectedCategory.indexOf(type) == -1)
+      setSelectedCategory(selectedCategory => [...selectedCategory, type]);    
     } 
+
+    function handleGenderChange(type,value){
+      setGender(value);
+      if(selectedCategory.indexOf(type) == -1)
+      setSelectedCategory(selectedCategory => [...selectedCategory, type]);
+    }
 
     function onSelect(type,value){
         console.log("event",type,value);
         switch(type){
           case 'disease':
-            handleDiseaseChange(value);
+            handleDiseaseChange(type,value);
             break;
           case 'gender':
-            setGender(value)
+            handleGenderChange(type,value);
         }
     }
 
-
-    console.log("selected gender",gender);
-    console.log("gender count",genderCount);
-    // console.log("disease list",diseaseList);
+       console.log("Selected category------>", selectedCategory);
+    // console.log("selected gender",gender);
+    // console.log("gender count",genderCount);
 
 
   return (
@@ -127,8 +147,15 @@ export default function PatientStatistics() {
 
         <Col xs={6} md={4}>
         <DropdownButton title={gender}>
-        <MenuItem eventKey="1" onSelect={() => onSelect("gender","Male")}>Male</MenuItem>
-       <MenuItem eventKey="2" onSelect={() => onSelect("gender","Female")}>Female</MenuItem>
+        {   
+          Object.keys(genderCount).map((key,value)=>{
+            return (
+          <MenuItem eventKey={value} onSelect={() => onSelect("gender",key)}>{key}</MenuItem>
+              )
+          })
+        }
+        {/* <MenuItem eventKey="1" onSelect={() => onSelect("gender","Male")}>Male</MenuItem>
+       <MenuItem eventKey="2" onSelect={() => onSelect("gender","Female")}>Female</MenuItem> */}
         </DropdownButton>
         </Col>            
        
@@ -152,12 +179,6 @@ export default function PatientStatistics() {
               )
           })
         }
-        {/* {diseaseList.map(ailment => {              
-              return(
-                  <MenuItem eventKey={ailment} onSelect={() => onSelect("disease",ailment)}>{ailment}</MenuItem>
-              );
-            })} */}
-
         </DropdownButton>
         </Col>            
        
